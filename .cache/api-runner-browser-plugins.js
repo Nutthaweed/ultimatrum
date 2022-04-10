c1dc5b0c9f2328ab6a9ba88016eaa8bd44a6a75b
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2022-04-11 01:23:33 +0700

    commit

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-styled-components/gatsby-browser.js'),
-      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"topLevelImportPaths":[],"pure":false,"disableVendorPrefixes":false},
-    }]
